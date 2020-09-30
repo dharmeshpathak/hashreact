942a08ae7f9bb6d2b4c49e599cb78fe850455283
Nitesh Sharma <niteshsharma312000@gmail.com>
2020-09-30 21:14:17 +0530

    Updated Styling of Default Widgets

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  const contentStyle = {
+    width: "100%";
+    minWidth: "300px";
+    maxWidth: "400px";
+    margin: "2% auto";
+    background: "#fff";
+    padding: "2%";
+    boxShadow: "1px 1px 6px -2px rgba(0, 0, 0, .3)";
+    textAlign: "center";
+  }
+  
+  const msgStyle = {
+    padding: "10px";
+    margin: "10px";
+    backgroundColor: "#f4f4f4";
+    border: 1px solid "#ccc";
+  
+  }
+  
+
+  render() {
+
+    return (
+      <div>
+        <div style = {contentStyle}>
+        <h1> Simple Hash Change</h1>
+          <h3>Direct links</h3>
+        <ul>
+          <li><a href="#1">Click here for 1</a></li>
+          <li><a href="#2">Click here for 2</a></li>
+          <li><a href="#3">Click here for 3</a></li>
+        </ul>
+        <h3>OnClick</h3>
+        <button id="js-button" data-test="4">Click here for 4</button>
+        <div style={msgStyle} id="js-msg"></div>
+      </div>
+      </div>    
+    ); 
+  }
 }
 
-export default App;
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
+
+exports default App;
